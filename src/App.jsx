0638744966682352d718b1864f4ee009c499bfe1
harshil114 <harshilvispute114@gmail.com>
2024-06-07 20:23:16 +0530

    setting up files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import LeftMainSection from "./components/LeftMainSection"
+
+function App() {
+  return (
+    <>
+    <LeftMainSection/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LeftMainSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import heroImg from '../assets/facebook-hero.svg'
+
+export default function LeftMainSection() {
+  return (
+    <div>
+        <img src={heroImg} alt="heroImg" />      
+    </div>
+  )
+}
