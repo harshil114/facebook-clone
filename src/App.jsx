5368e0053fe5daac5ace05c50902e7317260b717
harshil114 <harshilvispute114@gmail.com>
2024-06-09 12:32:37 +0530

    refactor: clean up code formatting and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import LeftMainSection from "./components/LeftMainSection"
-import RightMainSection from "./components/RightMainSection"
-
+import LeftMainSection from "./components/LeftMainSection";
+import RightMainSection from "./components/RightMainSection";
 
 function App() {
   return (
-    <>
-    <div className="container flex justify-center items-center mx-auto mt-28" >
-      <LeftMainSection/>
-      <RightMainSection/>
-
+    <div className="container flex justify-center items-center mx-auto mt-28">
+      <LeftMainSection />
+      <RightMainSection />
     </div>
- 
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/LeftMainSection.jsx
+++ src/components/LeftMainSection.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import heroImg from "../assets/facebook-hero.svg";
-import "../index.css";
 
 export default function LeftMainSection() {
   return (
-    <>
-      <div className="w-1/2">
-        <img src={heroImg} alt="heroImg" className="w-80" />
-        <p className="ml-6 text-3xl w-10/12">Facebook helps you connect and share with the people in your life.</p>
-      </div>
-    </>
+    <div className="w-1/2">
+      <img src={heroImg} alt="heroImg" className="w-80" />
+      <p className="ml-6 text-3xl w-10/12">
+        Facebook helps you connect and share with the people in your life.
+      </p>
+    </div>
   );
 }
