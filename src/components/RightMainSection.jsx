885c81a4f34998ce402803367d422c0d5bfda297
harshil114 <harshilvispute114@gmail.com>
2024-06-09 12:53:41 +0530

    style: update button colors and add effects

--- src/components/RightMainSection.jsx
+++ src/components/RightMainSection.jsx
@@ -15,15 +15,15 @@ export default function RightMainSection() {
           placeholder="password"
         />
       </div>
-      <button className="bg-blue-600 text-white p-2 mx-3 rounded-lg font-bold text-lg">
+      <button className="bg-[#0866ff] text-white p-2 mx-3 rounded-lg font-medium text-2xl">
         Log in
       </button>
       <span className="text-center text-blue-600 my-4 tracking-tighter hover:cursor-pointer hover:underline">
         Forgotten password?
       </span>
       <hr className="mx-3" />
-      <button className="bg-green-600 text-white p-3 mx-auto my-6  rounded-lg font-bold text-lg">
-        Create new Account
+      <button className="bg-[#42b72a] text-white py-2.5 px-3 mx-auto my-6 font-medium rounded-lg text-lg hover:bg-[#3ba225] active:bg-[#42b72a]">
+        Create new account
       </button>
       <div className="text-center w-1/4">
         <span class="absolute -bottom-12 text-sm">
